d8ddc793ef37f6afcec6579b7f6f49ed4c466245
Tim Bentley <tim.bentley@gmail.com>
2009-03-19 17:36:06 +0000

    Remove PluginUtils as no longer used all references migrated

--- openlp/core/lib/__init__.py
+++ openlp/core/lib/__init__.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 """
-from pluginutils import PluginUtils
 from pluginconfig import PluginConfig
 from plugin import Plugin
 from settingstab import SettingsTab
@@ -31,6 +30,6 @@ from toolbar import OpenLPToolbar
 from songxmlhandler import SongXMLBuilder
 from songxmlhandler import SongXMLParser
 
-__all__ = ['PluginConfig', 'Plugin', 'PluginUtils', 'SettingsTab', 'MediaManagerItem', 'Event',
+__all__ = ['PluginConfig', 'Plugin', 'SettingsTab', 'MediaManagerItem', 'Event',
            'XmlRootClass', 'ServiceItem', 'Receiver', 'OpenLPToolbar', 'SongXMLBuilder', 
            'SongXMLParser']
