64a988f8871f66136a1d00873ead6cea8da4a49f
Tim Bentley <tim.bentley@gmail.com>
2009-05-02 12:16:08 +0100

    At last first attemp at serviceitem

--- openlp/core/lib/serviceitem.py
+++ openlp/core/lib/serviceitem.py
@@ -3,7 +3,7 @@
 """
 OpenLP - Open Source Lyrics Projection
 Copyright (c) 2008 Raoul Snyman
-Portions copyright (c) 2008 Martin Thompson, Tim Bentley
+Portions copyright (c) 2008-2009 Martin Thompson, Tim Bentley
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -17,31 +17,55 @@ You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 """
+import logging
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class ServiceItem(QAbstractItemModel):
+#was Qabstratctitemodel
+class ServiceItem():
     """
     The service item is a base class for the plugins to use to interact with
     the service manager, the slide controller, and the projection screen
     compositor.
     """
+    global log
+    log=logging.getLogger(u'ServiceItem')
+    log.info(u'Service Item created')
 
     def __init__(self, hostplugin):
         """
         Init Method
         """
-        self.plugin=hostplugin
-        self.shortname=hostplugin.name
-        self.items=[]
-        self.iconic_representation=None
-    
+
+        self.plugin = hostplugin
+        self.shortname = hostplugin.name
+        self.items = []
+        self.iconic_representation = None
+        self.raw_slides = None
+        self.format_slides = []
+        self.frames = []
+        self.raw_footer = None
+        self.theme = None
+        log.debug(u'Service item created for %s', self.shortname)
+
     def render(self):
         """
         The render method is what the plugin uses to render it's meda to the
         screen.
         """
-        pass
+        log.debug(u'Render called')
+        if self.theme == None:
+            self.render_manager.set_override_theme(None)
+        else:
+            self.render_manager.set_override_theme(self.theme)
+
+        for slide in self.raw_slides:
+            self.format_slides.append(self.render_manager.format_slide(slide, False))
+        for slide in self.format_slides:
+
+            self.frames.append(self.render_manager.generate_slide(slide, self.raw_footer, False))
+        print self.frames
 
     def get_parent_node(self):
         """
@@ -64,7 +88,7 @@ class ServiceItem(QAbstractItemModel):
         """
         pass
 
-    def set_from_plugin(self, data):
+    def set_from_plugin(self):
         """
         Takes data from the plugin media chooser
         """
