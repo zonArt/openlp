1006d4e3aa67754e12a3f6c9d51f5dde55b265c6
Tim Bentley <tim.bentley@gmail.com>
2009-07-15 18:33:31 +0100

    Clean up phase 1 one of render sort out fix bug in render frames (who forgot the footer)

--- openlp/core/lib/rendermanager.py
+++ openlp/core/lib/rendermanager.py
@@ -138,12 +138,12 @@ class RenderManager(object):
         main_rect = None
         footer_rect = None
         if theme.font_main_override == False:
-            main_rect = QtCore.QRect(10,0, self.width-1, self.height-1)
+            main_rect = QtCore.QRect(10,0, self.width - 1,  self.footer_start - 20)
         else:
             main_rect = QtCore.QRect(int(theme.font_main_x) , int(theme.font_main_y),
-                int(theme.font_main_width)-1, int(theme.font_main_height)-1)
+                int(theme.font_main_width)-1, int(theme.font_main_height) - 1)
         if theme.font_footer_override == False:
-            footer_rect = QtCore.QRect(10,self.footer_start, self.width-1, self.height-self.footer_start)
+            footer_rect = QtCore.QRect(10,self.footer_start, self.width - 1, self.height-self.footer_start)
         else:
             footer_rect = QtCore.QRect(int(theme.font_footer_x),int(theme.font_footer_y),
                 int(theme.font_footer_width)-1, int(theme.font_footer_height)-1)
