10e888471f21bcc875018d1a89477fc10ef67b7b
Tim Bentley <tim.bentley@gmail.com>
2009-09-23 19:49:26 +0100

    Minor changes and clean ups

--- openlp/core/ui/mainwindow.py
+++ openlp/core/ui/mainwindow.py
@@ -405,11 +405,11 @@ class Ui_MainWindow(object):
         self.action_Preview_Panel.setStatusTip(translate(u'mainWindow',
             u'Toggle the visibility of the Preview Panel'))
         self.action_Preview_Panel.setShortcut(translate(u'mainWindow', u'F11'))
-        self.ToolsAlertItem.setText(translate(u'mainWindow', u'Trigger &Alert'))
+        self.ToolsAlertItem.setText(translate(u'mainWindow', u'Nursery &Alert'))
         self.ToolsAlertItem.setStatusTip(
             translate(u'mainWindow', u'Show an alert message'))
         self.ToolsAlertItem.setShortcut(translate(u'mainWindow', u'F7'))
-        self.PluginItem.setText(translate(u'mainWindow', u'List &Plugins'))
+        self.PluginItem.setText(translate(u'mainWindow', u'&Plugin List'))
         self.PluginItem.setStatusTip(
             translate(u'mainWindow', u'List the Plugins'))
         self.PluginItem.setShortcut(translate(u'mainWindow', u'Alt+F7'))
