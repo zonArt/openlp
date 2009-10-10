7a98f16f6e1d4384e2ab9dd4e116723991bb3f94
Tim Bentley <tim.bentley@gmail.com>
2009-10-10 14:13:57 +0100

    More hiding changes

--- openlp/plugins/custom/customplugin.py
+++ openlp/plugins/custom/customplugin.py
@@ -56,5 +56,17 @@ class CustomPlugin(Plugin):
         # Create the CustomManagerItem object
         return CustomMediaItem(self, self.icon, u'Custom Slides')
 
+    def can_be_disabled(self):
+        return True
+
+    def initialise(self):
+        log.info(u'Plugin Initialising')
+        Plugin.initialise(self)
+        self.insert_toolbox_item()
+
+    def finalise(self):
+        log.info(u'Plugin Finalise')
+        self.remove_toolbox_item()
+
     def about(self):
         return u'<b>Custom Plugin</b> <br>This plugin allows slides to be displayed on the screen in the same way songs are. The difference between this plugin and songs is this plugin provides greater freedom.<br><br>This is a core plugin and cannot be made inactive</b>'
