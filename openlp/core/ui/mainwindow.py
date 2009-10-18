c5f9ac27cb3686a1da2d6b9c6dc2d20e8f537950
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2009-10-18 17:09:09 +0200

    Lighten the gradient of the toolbox titles.

--- openlp/core/ui/mainwindow.py
+++ openlp/core/ui/mainwindow.py
@@ -38,7 +38,7 @@ from openlp.core.utils import check_latest_version
 media_manager_style = """
   QToolBox::tab {
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-        stop: 0 palette(midlight), stop: 1.0 palette(dark));
+        stop: 0 palette(midlight), stop: 1.0 palette(mid));
     border-width: 1px;
     border-style: outset;
     border-color: palette(midlight);
@@ -46,7 +46,7 @@ media_manager_style = """
   }
   QToolBox::tab:selected {
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-        stop: 0 palette(light), stop: 1.0 palette(dark));
+        stop: 0 palette(light), stop: 1.0 palette(mid));
     border-color: palette(light);
   }
 """
