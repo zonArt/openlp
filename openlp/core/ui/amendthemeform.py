63028ee1d88c2f74b4072d9425bf339a1fa9d862
Michael Gorven <michael@gorven.za.net>
2009-11-06 11:52:04 +0200

    Change to Unicode string.

--- openlp/core/ui/amendthemeform.py
+++ openlp/core/ui/amendthemeform.py
@@ -200,7 +200,7 @@ class AmendThemeForm(QtGui.QDialog, Ui_AmendThemeDialog):
 
     def onImageToolButtonClicked(self):
         filename = QtGui.QFileDialog.getOpenFileName(
-            self, self.trUtf8('Open file'))
+            self, self.trUtf8(u'Open file'))
         if filename != u'':
             self.ImageLineEdit.setText(filename)
             self.theme.background_filename = filename
