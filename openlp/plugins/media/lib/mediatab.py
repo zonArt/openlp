c42af9327ac87ccbe813966faa69d859b3e4372c
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2009-11-30 20:26:37 +0200

    Removed "Video Mode Rendering" as it is no longer relevant (or even related!)

--- openlp/plugins/media/lib/mediatab.py
+++ openlp/plugins/media/lib/mediatab.py
@@ -36,45 +36,15 @@ class MediaTab(SettingsTab):
     def setupUi(self):
         self.setObjectName(u'MediaTab')
         self.tabTitleVisible = self.trUtf8('Media')
-        self.MediaLayout = QtGui.QFormLayout(self)
-        self.MediaLayout.setObjectName(u'MediaLayout')
-        self.MediaModeGroupBox = QtGui.QGroupBox(self)
-        self.MediaModeGroupBox.setObjectName(u'MediaModeGroupBox')
-        self.MediaModeLayout = QtGui.QVBoxLayout(self.MediaModeGroupBox)
-        self.MediaModeLayout.setSpacing(8)
-        self.MediaModeLayout.setMargin(8)
-        self.MediaModeLayout.setObjectName(u'MediaModeLayout')
-        self.UseVMRCheckBox = QtGui.QCheckBox(self.MediaModeGroupBox)
-        self.UseVMRCheckBox.setObjectName(u'UseVMRCheckBox')
-        self.MediaModeLayout.addWidget(self.UseVMRCheckBox)
-        self.UseVMRLabel = QtGui.QLabel(self.MediaModeGroupBox)
-        self.UseVMRLabel.setObjectName(u'UseVMRLabel')
-        self.MediaModeLayout.addWidget(self.UseVMRLabel)
-
-        self.MediaLayout.setWidget(
-            0, QtGui.QFormLayout.LabelRole, self.MediaModeGroupBox)
-        # Signals and slots
-        QtCore.QObject.connect(self.UseVMRCheckBox,
-            QtCore.SIGNAL(u'stateChanged(int)'), self.onVMRCheckBoxChanged)
+        #self.MediaLayout = QtGui.QFormLayout(self)
+        #self.MediaLayout.setObjectName(u'MediaLayout')
 
     def retranslateUi(self):
-        self.MediaModeGroupBox.setTitle(self.trUtf8('Media Mode'))
-        self.UseVMRCheckBox.setText(self.trUtf8('Use Video Mode Rendering'))
-        self.UseVMRLabel.setText(
-            self.trUtf8('<em>No video preview available with VMR enabled</em>'))
-
-    def onVMRCheckBoxChanged(self):
-        use_vmr_mode = self.UseVMRCheckBox.checkState()
-        self.use_vmr_mode = False
-        if use_vmr_mode == 2:
-            # we have a set value convert to True/False
-            self.use_vmr_mode = True
+        pass
 
     def load(self):
-        self.use_vmr_mode = str_to_bool(
-            self.config.get_config(u'use mode layout', u'False'))
-        if self.use_vmr_mode :
-            self.UseVMRCheckBox.setChecked(True)
+        pass
 
     def save(self):
-        self.config.set_config(u'use mode layout', unicode(self.use_vmr_mode))
+        pass
+
