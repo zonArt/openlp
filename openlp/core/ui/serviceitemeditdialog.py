b939f774639c254739849bfa189a1b323470f326
Tim Bentley <tim.bentley@gmail.com>
2010-03-18 22:06:51 +0000

    Fix dialog

--- openlp/core/ui/serviceitemeditdialog.py
+++ openlp/core/ui/serviceitemeditdialog.py
@@ -1,62 +1,48 @@
 # -*- coding: utf-8 -*-
-# vim: autoindent shiftwidth=4 expandtab textwidth=80 tabstop=4 softtabstop=4
 
-###############################################################################
-# OpenLP - Open Source Lyrics Projection                                      #
-# --------------------------------------------------------------------------- #
-# Copyright (c) 2008-2010 Raoul Snyman                                        #
-# Portions copyright (c) 2008-2010 Tim Bentley, Jonathan Corwin, Michael      #
-# Gorven, Scott Guerrieri, Maikel Stuivenberg, Martin Thompson, Jon Tibble,   #
-# Carsten Tinggaard                                                           #
-# --------------------------------------------------------------------------- #
-# This program is free software; you can redistribute it and/or modify it     #
-# under the terms of the GNU General Public License as published by the Free  #
-# Software Foundation; version 2 of the License.                              #
-#                                                                             #
-# This program is distributed in the hope that it will be useful, but WITHOUT #
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
-# more details.                                                               #
-#                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with this program; if not, write to the Free Software Foundation, Inc., 59  #
-# Temple Place, Suite 330, Boston, MA 02111-1307 USA                          #
-###############################################################################
+# Form implementation generated from reading ui file 'serviceitemeditdialog.ui'
+#
+# Created: Thu Mar 18 22:05:22 2010
+#      by: PyQt4 UI code generator 4.7
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
 class Ui_ServiceItemEditDialog(object):
     def setupUi(self, ServiceItemEditDialog):
         ServiceItemEditDialog.setObjectName("ServiceItemEditDialog")
-        ServiceItemEditDialog.resize(400, 287)
-        self.widget = QtGui.QWidget(ServiceItemEditDialog)
-        self.widget.setGeometry(QtCore.QRect(20, 20, 351, 241))
-        self.widget.setObjectName("widget")
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.listWidget = QtGui.QListWidget(self.widget)
+        ServiceItemEditDialog.resize(386, 272)
+        self.layoutWidget = QtGui.QWidget(ServiceItemEditDialog)
+        self.layoutWidget.setGeometry(QtCore.QRect(20, 20, 351, 241))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.outerLayout = QtGui.QVBoxLayout(self.layoutWidget)
+        self.outerLayout.setObjectName("outerLayout")
+        self.topLayout = QtGui.QHBoxLayout()
+        self.topLayout.setObjectName("topLayout")
+        self.listWidget = QtGui.QListWidget(self.layoutWidget)
         self.listWidget.setAlternatingRowColors(True)
         self.listWidget.setObjectName("listWidget")
-        self.horizontalLayout.addWidget(self.listWidget)
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.upButton = QtGui.QPushButton(self.widget)
+        self.topLayout.addWidget(self.listWidget)
+        self.buttonLayout = QtGui.QVBoxLayout()
+        self.buttonLayout.setObjectName("buttonLayout")
+        self.upButton = QtGui.QPushButton(self.layoutWidget)
         self.upButton.setObjectName("upButton")
-        self.verticalLayout.addWidget(self.upButton)
-        self.downButton = QtGui.QPushButton(self.widget)
-        self.downButton.setObjectName("downButton")
-        self.verticalLayout.addWidget(self.downButton)
-        self.deleteButton = QtGui.QPushButton(self.widget)
+        self.buttonLayout.addWidget(self.upButton)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.buttonLayout.addItem(spacerItem)
+        self.deleteButton = QtGui.QPushButton(self.layoutWidget)
         self.deleteButton.setObjectName("deleteButton")
-        self.verticalLayout.addWidget(self.deleteButton)
-        self.horizontalLayout.addLayout(self.verticalLayout)
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
-        self.buttonBox = QtGui.QDialogButtonBox(self.widget)
+        self.buttonLayout.addWidget(self.deleteButton)
+        self.downButton = QtGui.QPushButton(self.layoutWidget)
+        self.downButton.setObjectName("downButton")
+        self.buttonLayout.addWidget(self.downButton)
+        self.topLayout.addLayout(self.buttonLayout)
+        self.outerLayout.addLayout(self.topLayout)
+        self.buttonBox = QtGui.QDialogButtonBox(self.layoutWidget)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Save)
         self.buttonBox.setObjectName("buttonBox")
-        self.verticalLayout_2.addWidget(self.buttonBox)
+        self.outerLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(ServiceItemEditDialog)
         QtCore.QMetaObject.connectSlotsByName(ServiceItemEditDialog)
@@ -64,6 +50,6 @@ class Ui_ServiceItemEditDialog(object):
     def retranslateUi(self, ServiceItemEditDialog):
         ServiceItemEditDialog.setWindowTitle(QtGui.QApplication.translate("ServiceItemEditDialog", "Service Item Maintenance", None, QtGui.QApplication.UnicodeUTF8))
         self.upButton.setText(QtGui.QApplication.translate("ServiceItemEditDialog", "Up", None, QtGui.QApplication.UnicodeUTF8))
-        self.downButton.setText(QtGui.QApplication.translate("ServiceItemEditDialog", "Down", None, QtGui.QApplication.UnicodeUTF8))
         self.deleteButton.setText(QtGui.QApplication.translate("ServiceItemEditDialog", "Delete", None, QtGui.QApplication.UnicodeUTF8))
+        self.downButton.setText(QtGui.QApplication.translate("ServiceItemEditDialog", "Down", None, QtGui.QApplication.UnicodeUTF8))
 
