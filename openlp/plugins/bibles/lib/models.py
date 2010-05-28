9e3c5524b11b04999afd38fd4c097ab8dd9aadc2
Jon Tibble <meths@btinternet.com>
2010-05-28 01:26:49 +0100

    Refactor BaseModel

--- openlp/plugins/bibles/lib/models.py
+++ openlp/plugins/bibles/lib/models.py
@@ -27,20 +27,7 @@ from sqlalchemy import Column, Table, MetaData, ForeignKey, types, \
     create_engine
 from sqlalchemy.orm import mapper, relation, sessionmaker, scoped_session
 
-class BaseModel(object):
-    """
-    BaseModel provides a base object with a set of generic functions
-    """
-    @classmethod
-    def populate(cls, **kwargs):
-        """
-        Creates an instance of a class and populates it, returning the instance
-        """
-        me = cls()
-        keys = kwargs.keys()
-        for key in keys:
-            me.__setattr__(key, kwargs[key])
-        return me
+from openlp.core.lib import BaseModel
 
 
 class BibleMeta(BaseModel):
