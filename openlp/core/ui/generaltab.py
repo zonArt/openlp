bceb21acc34b888f9601f5b735f464f8bb0884cf
Frode Woldsund <frodus@frodus-laptop>
2010-06-19 01:48:16 +0200

    String Cleanup: replace translate(u'', u'') -> translate('', '')

--- openlp/core/ui/generaltab.py
+++ openlp/core/ui/generaltab.py
@@ -54,7 +54,7 @@ class GeneralTab(SettingsTab):
 
     def setupUi(self):
         self.setObjectName(u'GeneralTab')
-        self.tabTitleVisible = translate(u'GeneralTab', u'General')
+        self.tabTitleVisible = translate('GeneralTab', 'General')
         self.GeneralLayout = QtGui.QHBoxLayout(self)
         self.GeneralLayout.setSpacing(8)
         self.GeneralLayout.setMargin(8)
@@ -177,32 +177,32 @@ class GeneralTab(SettingsTab):
             QtCore.SIGNAL(u'editingFinished()'), self.onPasswordEditLostFocus)
 
     def retranslateUi(self):
-        self.MonitorGroupBox.setTitle(translate(u'GeneralTab', u'Monitors'))
+        self.MonitorGroupBox.setTitle(translate('GeneralTab', 'Monitors'))
         self.MonitorLabel.setText(
-            translate(u'GeneralTab', u'Select monitor for output display:'))
+            translate('GeneralTab', 'Select monitor for output display:'))
         self.DisplayOnMonitorCheck.setText(
-            translate(u'GeneralTab', u'Display if a single screen'))
+            translate('GeneralTab', 'Display if a single screen'))
         self.StartupGroupBox.setTitle(
-            translate(u'GeneralTab', u'Application Startup'))
+            translate('GeneralTab', 'Application Startup'))
         self.WarningCheckBox.setText(
-            translate(u'GeneralTab', u'Show blank screen warning'))
+            translate('GeneralTab', 'Show blank screen warning'))
         self.AutoOpenCheckBox.setText(
-            translate(u'GeneralTab', u'Automatically open the last service'))
+            translate('GeneralTab', 'Automatically open the last service'))
         self.ShowSplashCheckBox.setText(
-            translate(u'GeneralTab', u'Show the splash screen'))
+            translate('GeneralTab', 'Show the splash screen'))
         self.SettingsGroupBox.setTitle(
-            translate(u'GeneralTab', u'Application Settings'))
+            translate('GeneralTab', 'Application Settings'))
         self.SaveCheckServiceCheckBox.setText(
-            translate(u'GeneralTab', 
-            u'Prompt to save Service before starting New'))
+            translate('GeneralTab', 
+            'Prompt to save Service before starting New'))
         self.AutoPreviewCheckBox.setText(
-            translate(u'GeneralTab', u'Preview Next Song from Service Manager'))
-        self.CCLIGroupBox.setTitle(translate(u'GeneralTab', u'CCLI Details'))
-        self.NumberLabel.setText(translate(u'GeneralTab', u'CCLI Number:'))
+            translate('GeneralTab', 'Preview Next Song from Service Manager'))
+        self.CCLIGroupBox.setTitle(translate('GeneralTab', 'CCLI Details'))
+        self.NumberLabel.setText(translate('GeneralTab', 'CCLI Number:'))
         self.UsernameLabel.setText(
-            translate(u'GeneralTab', u'SongSelect Username:'))
+            translate('GeneralTab', 'SongSelect Username:'))
         self.PasswordLabel.setText(
-            translate(u'GeneralTab', u'SongSelect Password:'))
+            translate('GeneralTab', 'SongSelect Password:'))
 
     def onMonitorComboBoxChanged(self):
         self.MonitorNumber = self.MonitorComboBox.currentIndex()
@@ -238,11 +238,11 @@ class GeneralTab(SettingsTab):
         settings = QtCore.QSettings()
         settings.beginGroup(self.settingsSection)
         for screen in self.screens.screen_list:
-            screen_name = u'%s %d' % (translate(u'GeneralTab', u'Screen'),
+            screen_name = u'%s %d' % (translate('GeneralTab', 'Screen'),
                 screen[u'number'] + 1)
             if screen[u'primary']:
                 screen_name = u'%s (%s)' % (screen_name, 
-                    translate(u'GeneralTab', u'primary'))
+                    translate('GeneralTab', 'primary'))
             self.MonitorComboBox.addItem(screen_name)
         # Get the configs
         self.Warning = settings.value(
