6c4336e4db10b3573b765560a826e6950d8c8f70
Jon Tibble <meths@btinternet.com>
2010-06-29 17:07:45 +0100

    Fix service loading

--- openlp/core/ui/servicemanager.py
+++ openlp/core/ui/servicemanager.py
@@ -662,7 +662,7 @@ class ServiceManager(QtGui.QWidget):
         name = filename.split(os.path.sep)
         if filename:
             SettingsManager.set_last_dir(self.parent.serviceSettingsSection,
-                os.path.split(filename)[0])
+                name[0])
             zip = None
             file_to = None
             try:
