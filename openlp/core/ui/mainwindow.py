493af31c201f710402ac53807322f499e5ca8265
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2010-07-22 22:01:03 +0200

    Some newlines to make Jon T happy.

--- openlp/core/ui/mainwindow.py
+++ openlp/core/ui/mainwindow.py
@@ -44,13 +44,15 @@ MEDIA_MANAGER_STYLE = """
   }
   QToolBox::tab {
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-        stop: 0 palette(button), stop: 0.5 palette(button), stop: 1.0 palette(mid));
+        stop: 0 palette(button), stop: 0.5 palette(button),
+        stop: 1.0 palette(mid));
     border: 1px groove palette(mid);
     border-radius: 5px;
   }
   QToolBox::tab:selected {
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-        stop: 0 palette(light), stop: 0.5 palette(midlight), stop: 1.0 palette(dark));
+        stop: 0 palette(light), stop: 0.5 palette(midlight),
+        stop: 1.0 palette(dark));
     border: 1px groove palette(dark);
     font-weight: bold;
   }
