2794cf2c64797146610aa074e17ebe2c11d0a402
Jon Tibble <meths@btinternet.com>
2010-07-27 12:21:11 +0100

    EditCustom & SongUsage naming

--- openlp/plugins/songusage/forms/songusagedeleteform.py
+++ openlp/plugins/songusage/forms/songusagedeleteform.py
@@ -48,12 +48,11 @@ class SongUsageDeleteForm(QtGui.QDialog, Ui_SongUsageDeleteDialog):
                 'Delete Selected Song Usage Events?'),
             translate('SongUsagePlugin.SongUsageDeleteForm',
                 'Are you sure you want to delete selected Song Usage data?'),
-            QtGui.QMessageBox.StandardButtons(
-                QtGui.QMessageBox.Ok |
+            QtGui.QMessageBox.StandardButtons(QtGui.QMessageBox.Ok |
                 QtGui.QMessageBox.Cancel),
             QtGui.QMessageBox.Cancel)
         if ret == QtGui.QMessageBox.Ok:
-            deleteDate = self.DeleteCalendar.selectedDate().toPyDate()
+            deleteDate = self.deleteCalendar.selectedDate().toPyDate()
             self.songusagemanager.delete_all_objects(SongUsageItem,
                 SongUsageItem.usagedate <= deleteDate)
         self.close()
