47e00d09f5abc7146875bcb6fdbe6ab5a0ed70d5
Tim Bentley <tim.bentley@gmail.com>
2010-11-01 19:22:00 +0000

    Fix preview bug

--- openlp/core/ui/slidecontroller.py
+++ openlp/core/ui/slidecontroller.py
@@ -794,10 +794,11 @@ class SlideController(QtGui.QWidget):
         row = self.PreviewListWidget.currentRow()
         self.selectedRow = 0
         if row > -1 and row < self.PreviewListWidget.rowCount():
-            if self.serviceItem.is_command() and self.isLive:
-                Receiver.send_message(u'%s_slide' % self.serviceItem.name.lower(),
-                    [self.serviceItem, self.isLive, row])
-                self.updatePreview()
+            if self.serviceItem.is_command():
+                if self.isLive:
+                    Receiver.send_message(u'%s_slide' % self.serviceItem.name.lower(),
+                        [self.serviceItem, self.isLive, row])
+                    self.updatePreview()
             else:
                 toDisplay = self.serviceItem.get_rendered_frame(row)
                 if self.serviceItem.is_text():
