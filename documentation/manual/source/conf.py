b894ddbbd8d91729f2045c46382760a9cb9c283c
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2010-12-10 14:52:43 +0200

    Added a qthelp theme, updated the manual to work better as Qt  help.

--- documentation/manual/source/conf.py
+++ documentation/manual/source/conf.py
@@ -92,22 +92,22 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'openlp_qthelp'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'sidebarbgcolor': '#3a60a9',
-    'relbarbgcolor': '#203b6f',
-    'footerbgcolor': '#26437c',
-    'headtextcolor': '#203b6f',
-    'linkcolor': '#26437c',
-    'sidebarlinkcolor': '#ceceff'
-}
+#html_theme_options = {
+#    'sidebarbgcolor': '#3a60a9',
+#    'relbarbgcolor': '#203b6f',
+#    'footerbgcolor': '#26437c',
+#    'headtextcolor': '#203b6f',
+#    'linkcolor': '#26437c',
+#    'sidebarlinkcolor': '#ceceff'
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [u'../themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
