32bd23f18f1e36c1e049f7ce9b99847a2007e0d1
Andreas Preikschat <googol@hush.com>
2011-01-18 21:15:56 +0100

    addes spaces

--- openlp/core/lib/mediamanageritem.py
+++ openlp/core/lib/mediamanageritem.py
@@ -194,13 +194,13 @@ class MediaManagerItem(QtGui.QWidget):
         """
         # Add a toolbar
         self.addToolbar()
-        #Allow the plugin to define buttons at start of bar
+        # Allow the plugin to define buttons at start of bar
         self.addStartHeaderBar()
-        #Add the middle of the tool bar (pre defined)
+        # Add the middle of the tool bar (pre defined)
         self.addMiddleHeaderBar()
-        #Allow the plugin to define buttons at end of bar
+        # Allow the plugin to define buttons at end of bar
         self.addEndHeaderBar()
-        #Add the list view
+        # Add the list view
         self.addListViewToToolBar()
 
     def addMiddleHeaderBar(self):
