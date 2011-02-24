c95afa219dfb455325f51a44e1d859277f28d27e
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2011-02-24 07:47:38 +0200

    Updated copyright information. Removed .eric4project directory from version control.

--- openlp/plugins/songs/lib/xml.py
+++ openlp/plugins/songs/lib/xml.py
@@ -6,9 +6,9 @@
 # --------------------------------------------------------------------------- #
 # Copyright (c) 2008-2011 Raoul Snyman                                        #
 # Portions copyright (c) 2008-2011 Tim Bentley, Jonathan Corwin, Michael      #
-# Gorven, Scott Guerrieri, Meinert Jordan, Andreas Preikschat, Christian      #
-# Richter, Philip Ridout, Maikel Stuivenberg, Martin Thompson, Jon Tibble,    #
-# Carsten Tinggaard, Frode Woldsund                                           #
+# Gorven, Scott Guerrieri, Meinert Jordan, Armin Köhler, Andreas Preikschat,  #
+# Christian Richter, Philip Ridout, Maikel Stuivenberg, Martin Thompson, Jon  #
+# Tibble, Carsten Tinggaard, Frode Woldsund                                   #
 # --------------------------------------------------------------------------- #
 # This program is free software; you can redistribute it and/or modify it     #
 # under the terms of the GNU General Public License as published by the Free  #
@@ -409,7 +409,7 @@ class OpenLyrics(object):
             The song object.
         """
         if hasattr(properties, u'comments'):
-            comments_list = []  
+            comments_list = []
             for comment in properties.comments.comment:
                 commenttext = self._text(comment)
                 if commenttext:
