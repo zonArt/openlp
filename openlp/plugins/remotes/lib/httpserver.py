763bb7ca0510f7c86edf9e79679e7cf6daf38025
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2011-03-15 09:45:29 +0200

    Re-added my documentation which got lost somewhere along the line.

--- openlp/plugins/remotes/lib/httpserver.py
+++ openlp/plugins/remotes/lib/httpserver.py
@@ -24,6 +24,74 @@
 # Temple Place, Suite 330, Boston, MA 02111-1307 USA                          #
 ###############################################################################
 
+"""
+The :mod:`http` module contains the API web server. This is a lightweight web
+server used by remotes to interact with OpenLP. It uses JSON to communicate with
+the remotes.
+
+*Routes:*
+
+``/``
+    Go to the web interface.
+
+``/files/{filename}``
+    Serve a static file.
+
+``/api/poll``
+    Poll to see if there are any changes. Returns a JSON-encoded dict of
+    any changes that occurred::
+
+        {"results": {"type": "controller"}}
+
+    Or, if there were no results, False::
+
+        {"results": False}
+
+``/api/controller/{live|preview}/{action}``
+    Perform ``{action}`` on the live or preview controller. Valid actions
+    are:
+
+    ``next``
+        Load the next slide.
+
+    ``previous``
+        Load the previous slide.
+
+    ``jump``
+        Jump to a specific slide. Requires an id return in a JSON-encoded
+        dict like so::
+
+            {"request": {"id": 1}}
+
+    ``first``
+        Load the first slide.
+
+    ``last``
+        Load the last slide.
+
+    ``text``
+        Request the text of the current slide.
+
+``/api/service/{action}``
+    Perform ``{action}`` on the service manager (e.g. go live). Data is
+    passed as a json-encoded ``data`` parameter. Valid actions are:
+
+    ``next``
+        Load the next item in the service.
+
+    ``previous``
+        Load the previews item in the service.
+
+    ``jump``
+        Jump to a specific item in the service. Requires an id returned in
+        a JSON-encoded dict like so::
+
+            {"request": {"id": 1}}
+
+    ``list``
+        Request a list of items in the service.
+"""
+
 import logging
 import os
 import urlparse
