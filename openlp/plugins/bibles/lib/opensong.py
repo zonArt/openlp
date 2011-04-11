e4d658f0303c05e1d495cb44881955ae7b69f7c1
Armin Köhler <orangeshirt@web.de>
2011-04-11 22:57:00 +0200

    small fix

--- openlp/plugins/bibles/lib/opensong.py
+++ openlp/plugins/bibles/lib/opensong.py
@@ -1,4 +1,4 @@
-song# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # vim: autoindent shiftwidth=4 expandtab textwidth=80 tabstop=4 softtabstop=4
 
 ###############################################################################
