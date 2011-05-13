dfa884581ecc5ab2456d08a7daf2c78d48bf005b
Andreas Preikschat <googol@lavabit.com>
2011-05-13 21:44:34 +0200

    Fixed traceback, when 'locked' and no dual bible selected.

--- openlp/plugins/bibles/lib/mediaitem.py
+++ openlp/plugins/bibles/lib/mediaitem.py
@@ -587,7 +587,12 @@ class BibleMediaItem(MediaManagerItem):
         Check if the first item is a second bible item or not.
         """
         bitem = self.listView.item(0)
-        item_second_bible = self._decodeQtObject(bitem, 'second_bible')
+        if not bitem.flags() & QtCore.Qt.ItemIsSelectable:
+            # The item is the "No Search Results" item.
+            self.listView.clear()
+            item_second_bible = None
+        else:
+            item_second_bible = self._decodeQtObject(bitem, 'second_bible')
         if item_second_bible and second_bible or not item_second_bible and \
             not second_bible:
             self.displayResults(bible, second_bible)
