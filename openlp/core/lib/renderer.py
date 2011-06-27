8ceba2e326074bcb0ac15253edbbf91af87e71e7
Andreas Preikschat <googol@lavabit.com>
2011-06-27 13:44:57 +0200

    replaced <br> by <br />

--- openlp/core/lib/renderer.py
+++ openlp/core/lib/renderer.py
@@ -219,7 +219,7 @@ class Renderer(object):
         """
         log.debug(u'format slide')
         # Add line endings after each line of text used for bibles.
-        line_end = u'<br>'
+        line_end = u'<br />'
         if item.is_capable(ItemCapabilities.NoLineBreaks):
             line_end = u' '
         # Bibles
@@ -240,7 +240,7 @@ class Renderer(object):
                         pages.extend(self._paginate_slide(lines, line_end))
         new_pages = []
         for page in pages:
-            while page.endswith(u'<br>'):
+            while page.endswith(u'<br />'):
                 page = page[:-4]
             new_pages.append(page)
         return new_pages
@@ -326,13 +326,13 @@ class Renderer(object):
             The text to be fitted on the slide split into lines.
 
         ``line_end``
-            The text added after each line. Either ``u' '`` or ``u'<br>``.
+            The text added after each line. Either ``u' '`` or ``u'<br />``.
         """
         log.debug(u'_paginate_slide - Start')
         formatted = []
         previous_html = u''
         previous_raw = u''
-        separator = u'<br>'
+        separator = u'<br />'
         html_lines = map(expand_tags, lines)
         html = self.page_shell + separator.join(html_lines) + HTML_END
         self.web.setHtml(html)
@@ -357,7 +357,7 @@ class Renderer(object):
             The words to be fitted on the slide split into lines.
 
         ``line_end``
-            The text added after each line. Either ``u' '`` or ``u'<br>``. This
+            The text added after each line. Either ``u' '`` or ``u'<br />``. This
             is needed for bibles.
         """
         log.debug(u'_paginate_slide_words - Start')
@@ -434,12 +434,12 @@ class Renderer(object):
             using the binary chop. The elements can contain display tags.
 
         ``separator``
-            The separator for the elements. For lines this is ``u'<br>'`` and
+            The separator for the elements. For lines this is ``u'<br />'`` and
             for words this is ``u' '``.
 
         ``line_end``
             The text added after each "element line". Either ``u' '`` or
-            ``u'<br>``. This is needed for bibles.
+            ``u'<br />``. This is needed for bibles.
         """
         smallest_index = 0
         highest_index = len(html_list) - 1
@@ -459,7 +459,7 @@ class Renderer(object):
             # We found the number of words which will fit.
             if smallest_index == index or highest_index == index:
                 index = smallest_index
-                formatted.append(previous_raw.rstrip(u'<br>') +
+                formatted.append(previous_raw.rstrip(u'<br />') +
                     separator.join(raw_list[:index + 1]))
                 previous_html = u''
                 previous_raw = u''
