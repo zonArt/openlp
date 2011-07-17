d56470cc38d87e16450e44027284621fabf0d442
Andreas Preikschat <googol@lavabit.com>
2011-07-17 15:15:13 +0200

    changed spaces

--- openlp/core/lib/renderer.py
+++ openlp/core/lib/renderer.py
@@ -311,8 +311,8 @@ class Renderer(object):
         self.web_frame = self.web.page().mainFrame()
         # Adjust width and height to account for shadow. outline done in css
         self.page_shell = u'<!DOCTYPE html><html><head><style>' \
-            u'*{margin: 0; padding: 0; border: 0;} '\
-            u'#main {position: absolute;top: 0px;%s %s}</style></head><body>' \
+            u'*{margin:0; padding:0; border:0;} '\
+            u'#main {position:absolute; top:0px; %s %s}</style></head><body>' \
             u'<div id="main">' % \
             (build_lyrics_format_css(self.theme_data, self.page_width,
             self.page_height), build_lyrics_outline_css(self.theme_data))
