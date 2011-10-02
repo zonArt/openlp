b678171b4bbbcd7ef334511f871f3f82f437f965
Tim Bentley <tim.bentley@gmail.com>
2011-10-02 20:57:42 +0100

    Minor performance improvement

--- openlp/core/lib/serviceitem.py
+++ openlp/core/lib/serviceitem.py
@@ -366,7 +366,7 @@ class ServiceItem(object):
         self.notes = other.notes
         # copy theme over if present.  Assumes overridden
         if other.theme is not None:
-            self.theme = other.theme
+            self.update_theme(other.theme)
         if self.is_capable(ItemCapabilities.HasBackgroundAudio):
             log.debug(self.background_audio)
 
