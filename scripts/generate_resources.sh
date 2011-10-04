17482d0e3bc93ec2806ece5c7e615ccaa06e2f3a
Andreas Preikschat <googol@lavabit.com>
2011-10-04 18:02:45 +0200

    removed not needed files + updated copyright

--- scripts/generate_resources.sh
+++ scripts/generate_resources.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+# -*- coding: utf-8 -*-
 # vim: autoindent shiftwidth=4 expandtab textwidth=80 tabstop=4 softtabstop=4
 
 ###############################################################################
 # OpenLP - Open Source Lyrics Projection                                      #
 # --------------------------------------------------------------------------- #
 # Copyright (c) 2008-2011 Raoul Snyman                                        #
-# Portions copyright (c) 2008-2011 Tim Bentley, Jonathan Corwin, Michael      #
-# Gorven, Scott Guerrieri, Matthias Hub, Meinert Jordan, Armin Köhler,        #
-# Andreas Preikschat, Mattias Põldaru, Christian Richter, Philip Ridout,      #
-# Jeffrey Smith, Maikel Stuivenberg, Martin Thompson, Jon Tibble, Frode       #
-# Woldsund                                                                    #
+# Portions copyright (c) 2008-2011 Tim Bentley, Gerald Britton, Jonathan      #
+# Corwin, Michael Gorven, Scott Guerrieri, Matthias Hub, Meinert Jordan,      #
+# Armin Köhler, Joshua Miller, Stevan Pettit, Andreas Preikschat, Mattias     #
+# Põldaru, Christian Richter, Philip Ridout, Simon Scudder, Jeffrey Smith,    #
+# Maikel Stuivenberg, Martin Thompson, Jon Tibble, Frode Woldsund             #
 # --------------------------------------------------------------------------- #
 # This program is free software; you can redistribute it and/or modify it     #
 # under the terms of the GNU General Public License as published by the Free  #
@@ -49,7 +49,7 @@ cat openlp/core/resources.py.new | sed '/# Created: /d;/#      by: /d' \
     > openlp/core/resources.py
 
 # Patch resources.py to OpenLP coding style
-patch --posix -s openlp/core/resources.py scripts/resources.patch
+#patch --posix -s openlp/core/resources.py scripts/resources.patch
 
 # Remove temporary file
 rm openlp/core/resources.py.new
