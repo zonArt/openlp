dacb5d2969ce1e6f917fdebe8cd74c6f7cb4ebad
Jon Tibble <meths@btinternet.com>
2012-03-11 21:24:22 +0000

    Unused imports

--- openlp/plugins/bibles/bibleplugin.py
+++ openlp/plugins/bibles/bibleplugin.py
@@ -27,7 +27,7 @@
 
 import logging
 
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
 
 from openlp.core.lib import Plugin, StringContent, build_icon, translate
 from openlp.core.lib.ui import create_action, UiStrings
