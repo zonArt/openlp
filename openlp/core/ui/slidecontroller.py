6eaf4ffe56aec9d4d5061bd9f07eb9d6f5eaab01
Tim Bentley <tim.bentley@gmail.com>
2012-09-07 17:58:13 +0100

    Try 2

--- openlp/core/ui/slidecontroller.py
+++ openlp/core/ui/slidecontroller.py
@@ -889,8 +889,6 @@ class SlideController(Controller):
             self.display.buildHtml(self.serviceItem)
         if serviceItem.is_media():
             self.onMediaStart(serviceItem)
-            if self.isLive:
-                Receiver.send_message('playbackPlay', [self, u'(False,)'])
         self.slideSelected(True)
         self.previewListWidget.setFocus()
         if old_item:
@@ -1349,7 +1347,7 @@ class SlideController(Controller):
         """
         log.debug(u'SlideController onMediaStart')
         file = os.path.join(item.get_frame_path(), item.get_frame_title())
-        self.mediaController.video(self, file, False, False)
+        self.mediaController.video(self, file, False, False, self.hideMode())
         if not self.isLive or self.mediaController.withLivePreview:
             self.previewDisplay.show()
             self.slidePreview.hide()
