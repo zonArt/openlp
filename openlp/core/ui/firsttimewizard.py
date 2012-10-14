485bc84d88bc113954fde430b6296730d6079c52
Martin Zibricky <mzibr.public@gmail.com>
2012-10-14 20:42:20 +0200

    Bug #1002038: Disable and hide presentation plugin on osx.

--- openlp/core/ui/firsttimewizard.py
+++ openlp/core/ui/firsttimewizard.py
@@ -82,13 +82,13 @@ class Ui_FirstTimeWizard(object):
         self.imageCheckBox.setChecked(True)
         self.imageCheckBox.setObjectName(u'imageCheckBox')
         self.pluginLayout.addWidget(self.imageCheckBox)
-        self.presentationCheckBox = QtGui.QCheckBox(self.pluginPage)
-        if sys.platform == "darwin":
-            self.presentationCheckBox.setChecked(False)
-        else:
+        # TODO Presentation plugin is not yet working on Mac OS X.
+        # For now just ignore it.
+        if sys.platform != 'darwin':
+            self.presentationCheckBox = QtGui.QCheckBox(self.pluginPage)
             self.presentationCheckBox.setChecked(True)
-        self.presentationCheckBox.setObjectName(u'presentationCheckBox')
-        self.pluginLayout.addWidget(self.presentationCheckBox)
+            self.presentationCheckBox.setObjectName(u'presentationCheckBox')
+            self.pluginLayout.addWidget(self.presentationCheckBox)
         self.mediaCheckBox = QtGui.QCheckBox(self.pluginPage)
         self.mediaCheckBox.setChecked(True)
         self.mediaCheckBox.setObjectName(u'mediaCheckBox')
@@ -214,9 +214,11 @@ class Ui_FirstTimeWizard(object):
         self.bibleCheckBox.setText(translate('OpenLP.FirstTimeWizard', 'Bible'))
         self.imageCheckBox.setText(translate('OpenLP.FirstTimeWizard',
             'Images'))
-        self.presentationCheckBox.setText(translate('OpenLP.FirstTimeWizard',
-            'Presentations'))
-        if sys.platform == "darwin":
+        # TODO Presentation plugin is not yet working on Mac OS X.
+        # For now just ignore it.
+        if sys.platform != 'darwin':
+            self.presentationCheckBox.setText(translate('OpenLP.FirstTimeWizard',
+                'Presentations'))
             self.presentationCheckBox.setEnabled(False)
         self.mediaCheckBox.setText(translate('OpenLP.FirstTimeWizard',
             'Media (Audio and Video)'))
