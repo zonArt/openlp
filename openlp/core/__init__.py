e6b1657d407d79019617dfd921ce142f2beefdc9
Patrick Zimmermann <patrick@zakweb.de>
2012-11-22 19:01:50 +0100

    Correct indentation.

--- openlp/core/__init__.py
+++ openlp/core/__init__.py
@@ -133,8 +133,8 @@ class OpenLP(QtGui.QApplication):
             base_color = self.palette().color(QtGui.QPalette.Active,
                 QtGui.QPalette.Base)
             application_stylesheet = \
-            u'* {alternate-background-color: ' + \
-            base_color.name() + ';}\n'
+                u'* {alternate-background-color: ' + \
+                base_color.name() + ';}\n'
             application_stylesheet += nt_repair_stylesheet
             self.setStyleSheet(application_stylesheet)
         # show the splashscreen
