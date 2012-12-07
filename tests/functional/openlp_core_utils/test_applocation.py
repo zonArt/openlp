f2dd22026f130b90411b0d464ad7c8cf09b92d04
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2012-12-07 22:52:55 +0200

    Moved the _get_frozen_path() test to the proper test case.

--- tests/functional/openlp_core_utils/test_applocation.py
+++ tests/functional/openlp_core_utils/test_applocation.py
@@ -11,22 +11,6 @@ class TestAppLocation(TestCase):
     """
     A test suite to test out various methods around the AppLocation class.
     """
-    def get_frozen_path_test(self):
-        """
-        Test the _get_frozen_path() function
-        """
-        with patch(u'openlp.core.utils.sys') as mocked_sys:
-            # GIVEN: The sys module "without" a "frozen" attribute
-            mocked_sys.frozen = None
-            # WHEN: We call _get_frozen_path() with two parameters
-            # THEN: The non-frozen parameter is returned
-            assert _get_frozen_path(u'frozen', u'not frozen') == u'not frozen', u'Should return "not frozen"'
-            # GIVEN: The sys module *with* a "frozen" attribute
-            mocked_sys.frozen = 1
-            # WHEN: We call _get_frozen_path() with two parameters
-            # THEN: The frozen parameter is returned
-            assert _get_frozen_path(u'frozen', u'not frozen') == u'frozen', u'Should return "frozen"'
-
     def get_data_path_test(self):
         """
         Test the AppLocation.get_data_path() method
--- tests/functional/openlp_core_utils/test_utils.py
+++ tests/functional/openlp_core_utils/test_utils.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 from mock import patch
 
-from openlp.core.utils import get_filesystem_encoding
+from openlp.core.utils import get_filesystem_encoding, _get_frozen_path
 
 class TestUtils(TestCase):
     """
@@ -16,4 +16,20 @@ class TestUtils(TestCase):
         Test the get_filesystem_encoding() function
         """
         assert False, u'This test needs to be written'
+
+    def get_frozen_path_test(self):
+        """
+        Test the _get_frozen_path() function
+        """
+        with patch(u'openlp.core.utils.sys') as mocked_sys:
+            # GIVEN: The sys module "without" a "frozen" attribute
+            mocked_sys.frozen = None
+            # WHEN: We call _get_frozen_path() with two parameters
+            # THEN: The non-frozen parameter is returned
+            assert _get_frozen_path(u'frozen', u'not frozen') == u'not frozen', u'Should return "not frozen"'
+            # GIVEN: The sys module *with* a "frozen" attribute
+            mocked_sys.frozen = 1
+            # WHEN: We call _get_frozen_path() with two parameters
+            # THEN: The frozen parameter is returned
+            assert _get_frozen_path(u'frozen', u'not frozen') == u'frozen', u'Should return "frozen"'
 
