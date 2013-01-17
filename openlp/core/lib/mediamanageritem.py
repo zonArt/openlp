e57909168f120d87b0f8a01948cc0682faabd015
Andreas Preikschat <googol@lavabit.com>
2013-01-17 23:22:05 +0100

    fixed images

--- openlp/core/lib/mediamanageritem.py
+++ openlp/core/lib/mediamanageritem.py
@@ -328,7 +328,7 @@ class MediaManagerItem(QtGui.QWidget):
         Add a file to the list widget to make it available for showing
         """
         files = QtGui.QFileDialog.getOpenFileNames(self, self.onNewPrompt,
-            Settings.value(u'last directory'), self.onNewFileMasks)
+            Settings().value(self.settingsSection + u'/last directory'), self.onNewFileMasks)
         log.info(u'New files(s) %s', files)
         if files:
             Receiver.send_message(u'cursor_busy')
@@ -382,7 +382,7 @@ class MediaManagerItem(QtGui.QWidget):
             self.listView.clear()
             self.loadList(full_list)
             last_dir = os.path.split(unicode(files[0]))[0]
-            Settings(self.settingsSection).setValue(u'last directory', last_dir)
+            Settings().setValue(self.settingsSection + u'/last directory', last_dir)
             SettingsManager.set_list(self.settingsSection,
                 self.settingsSection, self.getFileList())
         if duplicates_found:
