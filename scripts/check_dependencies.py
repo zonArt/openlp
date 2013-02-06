1f35288936c5370e7e9a9965f183c81dead04a45
Andreas Preikschat <googol@lavabit.com>
2013-02-06 21:42:09 +0100

    changed wording

--- scripts/check_dependencies.py
+++ scripts/check_dependencies.py
@@ -40,7 +40,7 @@ import os
 import sys
 from distutils.version import LooseVersion
 
-# If we import uno before nose this will greate a warning. Just try to import nose first to supress the warning.
+# If we try to import uno before nose this will greate a warning. Just try to import nose first to supress the warning.
 try:
     import nose
 except ImportError:
