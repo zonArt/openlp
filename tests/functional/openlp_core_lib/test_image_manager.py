b1ea09516f41e15024923c0bcf9ce79aedea9f8b
Tim Bentley <tim.bentley@gmail.com>
2013-02-13 19:10:41 +0000

    Add new tests to system

---
+++ tests/functional/openlp_core_lib/test_image_manager.py
@@ -0,0 +1,49 @@
+"""
+    Package to test the openlp.core.ui package.
+"""
+
+import os
+
+from unittest import TestCase
+
+from mock import MagicMock
+from openlp.core.lib import Registry, ImageManager, ScreenList
+from PyQt4 import QtCore, QtGui, QtTest
+
+TEST_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), u'..', u'..', u'resources'))
+
+class TestImageManager(TestCase):
+
+    def setUp(self):
+        """
+        Create the UI
+        """
+        Registry.create()
+        self.app = QtGui.QApplication([])
+        ScreenList.create(self.app.desktop())
+        self.image_manager = ImageManager()
+
+    def tearDown(self):
+        """
+        Delete all the C++ objects at the end so that we don't have a segfault
+        """
+        del self.app
+
+    def basic_image_manager_test(self):
+        """
+        Test the Image Manager setup basic functionality
+        """
+        # GIVEN: the an image add to the image manager
+        self.image_manager.add_image(TEST_PATH, u'church.jpg', None)
+
+        # WHEN the image is retrieved
+        image = self.image_manager.get_image(TEST_PATH, u'church.jpg')
+
+        # THEN returned record is a type of image
+        self.assertEqual(isinstance(image, QtGui.QImage), True, u'The returned field is an image')
+
+        # WHEN the image is retrieved has not been loaded
+        # THEN a KeyError is thrown
+        with self.assertRaises(KeyError) as context:
+            self.image_manager.get_image(TEST_PATH, u'church1.jpg')
+        self.assertNotEquals(context.exception[0], u'', u'KeyError exception should have been thrown for missing image')
---
+++ tests/interfaces/openlp_core_ui/test_servicemanager.py
@@ -0,0 +1,41 @@
+"""
+    Package to test the openlp.core.ui package.
+"""
+from unittest import TestCase
+
+from mock import MagicMock
+from openlp.core.lib import Registry, ScreenList
+from openlp.core.ui.mainwindow import MainWindow
+from PyQt4 import QtGui
+
+
+class TestStartNoteDialog(TestCase):
+
+    def setUp(self):
+        """
+        Create the UI
+        """
+        Registry.create()
+        self.app = QtGui.QApplication([])
+        ScreenList.create(self.app.desktop())
+        Registry().register(u'application', MagicMock())
+        self.main_window = MainWindow()
+        self.service_manager = Registry().get(u'service_manager')
+
+    def tearDown(self):
+        """
+        Delete all the C++ objects at the end so that we don't have a segfault
+        """
+        del self.main_window
+        del self.app
+
+    def basic_service_manager_test(self):
+        """
+        Test the Service Manager display functionality
+        """
+        # GIVEN: A New Service Manager instance
+
+        # WHEN I have an empty display
+        # THEN the count of items should be zero
+        self.assertEqual(self.service_manager.service_manager_list.topLevelItemCount(), 0,
+            u'There the service manager list is empty ')
