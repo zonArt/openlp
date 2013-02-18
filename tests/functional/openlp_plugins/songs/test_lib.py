a95c37cbba55c3aea8e673ead2d686d2cbee24e3
Patrick Zimmermann <patrick@zakweb.de>
2013-02-18 22:42:04 +0100

    Remove non-needed test __init__ file. Split up testfunctions.

--- tests/functional/openlp_plugins/songs/test_lib.py
+++ tests/functional/openlp_plugins/songs/test_lib.py
@@ -34,64 +34,82 @@ from mock import MagicMock
 from openlp.plugins.songs.lib.songcompare import songs_probably_equal
 
 class TestLib(TestCase):
+    def setUp(self):
+        """
+        Mock up two songs and provide a set of lyrics for the songs_probably_equal tests.
+        """
+        self.full_lyrics =u'''amazing grace how sweet the sound that saved a wretch like me i once was lost but now am
+            found was blind but now i see  twas grace that taught my heart to fear and grace my fears relieved how
+            precious did that grace appear the hour i first believed  through many dangers toils and snares i have already
+            come tis grace that brought me safe thus far and grace will lead me home'''
+        self.short_lyrics =u'''twas grace that taught my heart to fear and grace my fears relieved how precious did that
+            grace appear the hour i first believed'''
+        self.error_lyrics =u'''amazing how sweet the trumpet that saved a wrench like me i once was losst but now am
+            found waf blind but now i see  it was grace that taught my heart to fear and grace my fears relieved how
+            precious did that grace appppppppear the hour i first believedxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx snares i have
+            already come to this grace that brought me safe so far and grace will lead me home'''
+        self.different_lyrics=u'''on a hill far away stood an old rugged cross the emblem of suffering and shame and i love
+            that old cross where the dearest and best for a world of lost sinners was slain  so ill cherish the old rugged
+            cross till my trophies at last i lay down i will cling to the old rugged cross and exchange it some day for a
+            crown'''
+        self.song1 = MagicMock()
+        self.song2 = MagicMock()
 
-    def songs_probably_equal_test(self):
+    def songs_probably_equal_same_song_test(self):
         """
-        Test the songs_probably_equal function.
+        Test the songs_probably_equal function with twice the same song.
         """
-        full_lyrics =u'''amazing grace how sweet the sound that saved a wretch like me i once was lost but now am
-        found was blind but now i see  twas grace that taught my heart to fear and grace my fears relieved how
-        precious did that grace appear the hour i first believed  through many dangers toils and snares i have already
-        come tis grace that brought me safe thus far and grace will lead me home'''
-        short_lyrics =u'''twas grace that taught my heart to fear and grace my fears relieved how precious did that
-        grace appear the hour i first believed'''
-        error_lyrics =u'''amazing how sweet the trumpet that saved a wrench like me i once was losst but now am
-        found waf blind but now i see  it was grace that taught my heart to fear and grace my fears relieved how
-        precious did that grace appppppppear the hour i first believedxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx snares i have
-        already come to this grace that brought me safe so far and grace will lead me home'''
-        different_lyrics=u'''on a hill far away stood an old rugged cross the emblem of suffering and shame and i love
-        that old cross where the dearest and best for a world of lost sinners was slain  so ill cherish the old rugged
-        cross till my trophies at last i lay down i will cling to the old rugged cross and exchange it some day for a
-        crown'''
-        song1 = MagicMock()
-        song2 = MagicMock()
-        
         #GIVEN: Two equal songs.
-        song1.search_lyrics = full_lyrics
-        song2.search_lyrics = full_lyrics
+        self.song1.search_lyrics = self.full_lyrics
+        self.song2.search_lyrics = self.full_lyrics
         
         #WHEN: We compare those songs for equality.
-        result = songs_probably_equal(song1, song2)
+        result = songs_probably_equal(self.song1, self.song2)
         
         #THEN: The result should be True.
         assert result is True, u'The result should be True'
-        
+
+
+    def songs_probably_equal_short_song_test(self):
+        """
+        Test the songs_probably_equal function with a song and a shorter version of the same song.
+        """
         #GIVEN: A song and a short version of the same song.
-        song1.search_lyrics = full_lyrics
-        song2.search_lyrics = short_lyrics
+        self.song1.search_lyrics = self.full_lyrics
+        self.song2.search_lyrics = self.short_lyrics
         
         #WHEN: We compare those songs for equality.
-        result = songs_probably_equal(song1, song2)
+        result = songs_probably_equal(self.song1, self.song2)
         
         #THEN: The result should be True.
         assert result is True, u'The result should be True'
-        
+
+
+    def songs_probably_equal_error_song_test(self):
+        """
+        Test the songs_probably_equal function with a song and a  very erroneous version of the same song.
+        """
         #GIVEN: A song and the same song with lots of errors.
-        song1.search_lyrics = full_lyrics
-        song2.search_lyrics = error_lyrics
+        self.song1.search_lyrics = self.full_lyrics
+        self.song2.search_lyrics = self.error_lyrics
         
         #WHEN: We compare those songs for equality.
-        result = songs_probably_equal(song1, song2)
+        result = songs_probably_equal(self.song1, self.song2)
         
         #THEN: The result should be True.
         assert result is True, u'The result should be True'
-        
+
+
+    def songs_probably_equal_different_song_test(self):
+        """
+        Test the songs_probably_equal function with two different songs.
+        """
         #GIVEN: Two different songs.
-        song1.search_lyrics = full_lyrics
-        song2.search_lyrics = different_lyrics
+        self.song1.search_lyrics = self.full_lyrics
+        self.song2.search_lyrics = self.different_lyrics
         
         #WHEN: We compare those songs for equality.
-        result = songs_probably_equal(song1, song2)
+        result = songs_probably_equal(self.song1, self.song2)
         
         #THEN: The result should be False.
         assert result is False, u'The result should be False'
