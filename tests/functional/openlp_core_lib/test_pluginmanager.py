609dc25b89fb8c9e6dccd220ded5e9afdf4d463a
Tim Bentley <tim.bentley@gmail.com>
2013-02-27 21:23:46 +0000

    Fix merge comments

--- tests/functional/openlp_core_lib/test_pluginmanager.py
+++ tests/functional/openlp_core_lib/test_pluginmanager.py
@@ -91,22 +91,6 @@ class TestPluginManager(TestCase):
         # WHEN: We run hook_settings_tabs()
         plugin_manager.hook_settings_tabs()
 
-    def hook_settings_tabs_with_active_plugin_and_no_form_test(self):
-        """
-        Test running the hook_settings_tabs() method with an active plugin and no settings form
-        """
-        # GIVEN: A PluginManager instance and a list with a mocked up plugin whose status is set to Active
-        mocked_plugin = MagicMock()
-        mocked_plugin.status = PluginStatus.Active
-        plugin_manager = PluginManager()
-        plugin_manager.plugins = [mocked_plugin]
-
-        # WHEN: We run hook_settings_tabs()
-        plugin_manager.hook_settings_tabs()
-
-        # THEN: The createSettingsTab() method should have been called
-        mocked_plugin.createSettingsTab.assert_called_with(self.mocked_settings_form)
-
     def hook_settings_tabs_with_active_plugin_and_mocked_form_test(self):
         """
         Test running the hook_settings_tabs() method with an active plugin and a mocked settings form
