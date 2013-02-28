9b69448fdead88608145375dd2a6cc39b2f6761d
Andreas Preikschat <googol@lavabit.com>
2013-02-28 15:43:14 +0100

    always log the exception

--- openlp/core/__init__.py
+++ openlp/core/__init__.py
@@ -184,9 +184,7 @@ class OpenLP(QtGui.QApplication):
         ``traceback``
             A traceback object with the details of where the exception occurred.
         """
-        if not hasattr(self, u'main_window'):
-            log.exception(''.join(format_exception(exctype, value, traceback)))
-            return
+        log.exception(''.join(format_exception(exctype, value, traceback)))
         if not hasattr(self, u'exception_form'):
             self.exception_form = ExceptionForm(self.main_window)
         self.exception_form.exceptionTextEdit.setPlainText(''.join(format_exception(exctype, value, traceback)))
