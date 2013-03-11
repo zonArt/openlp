70f9897f4382a75cdc8ec53c1d30d43da7058230
Raoul Snyman <raoul.snyman@saturnlaboratories.co.za>
2013-03-11 10:07:17 +0200

    Cleanup EditVerseForm and friends, and expand the test.

--- tests/interfaces/openlp_plugins_songs_forms/test_editverseform.py
+++ tests/interfaces/openlp_plugins_songs_forms/test_editverseform.py
@@ -3,7 +3,7 @@ Package to test the openlp.plugins.songs.forms.editverseform package.
 """
 from unittest import TestCase
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui, QtTest
 
 from openlp.core.lib import Registry
 from openlp.plugins.songs.forms.editverseform import EditVerseForm
@@ -32,4 +32,19 @@ class TestEditVerseForm(TestCase):
         """
         Test the EditVerseForm defaults are correct
         """
-        self.assertEqual(self.form.verseTextEdit.toPlainText(), u'', u'The verse edit box is empty.')
+        # GIVEN: An EditVerseForm instance
+        # WHEN: The form is shown
+        # THEN: The default value is correct
+        self.assertEqual(self.form.verse_text_edit.toPlainText(), u'', u'The verse edit box is empty.')
+
+    def insert_verse_test(self):
+        """
+        Test that inserting a verse works as it should.
+        """
+        # GIVEN: An instance of the EditVerseForm
+        # WHEN: The Insert button is clicked
+        QtTest.QTest.mouseClick(self.form.insert_button, QtCore.Qt.LeftButton)
+
+        # THEN: The verse text edit should have a Verse:1 in it
+        self.assertIn(u'---[Verse:1]---', self.form.verse_text_edit.toPlainText(),
+                      u'The verse text edit should have a verse identifier')
