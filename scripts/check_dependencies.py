d4ca2bdefeb96f46a7fd567f462013a2260a50f5
Andreas Preikschat <googol@lavabit.com>
2013-06-16 21:42:50 +0200

    use isinstance instead of type

--- scripts/check_dependencies.py
+++ scripts/check_dependencies.py
@@ -98,9 +98,9 @@ OPTIONAL_MODULES = [
 w = sys.stdout.write
 
 def check_vers(version, required, text):
-    if type(version) is not str:
+    if not isinstance(version, str):
         version = '.'.join(map(str, version))
-    if type(required) is not str:
+    if not isinstance(required, str):
         required = '.'.join(map(str, required))
     w('  %s >= %s ...    ' % (text, required))
     if LooseVersion(version) >= LooseVersion(required):
