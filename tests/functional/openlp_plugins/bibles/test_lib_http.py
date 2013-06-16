219332836571bf7260962eb9c98b2cb8977cb1a6
Tim Bentley <tim.bentley@gmail.com>
2013-06-16 08:54:16 +0100

    Clean up slidecontroller

--- tests/functional/openlp_plugins/bibles/test_lib_http.py
+++ tests/functional/openlp_plugins/bibles/test_lib_http.py
@@ -3,10 +3,10 @@
 """
 
 from unittest import TestCase
-from mock import MagicMock, patch
+from mock import MagicMock
 
 from openlp.core.lib import Registry
-from openlp.plugins.bibles.lib.http import BGExtract
+from openlp.plugins.bibles.lib.http import BGExtract, CWExtract
 
 
 class TestBibleHTTP(TestCase):
@@ -19,9 +19,9 @@ class TestBibleHTTP(TestCase):
         Registry().register(u'service_list', MagicMock())
         Registry().register(u'application', MagicMock())
 
-    def bible_gateway_extract_test(self):
+    def bible_gateway_extract_books_test(self):
         """
-        Test the Bible Gateway retrieval of book list for NIV
+        Test the Bible Gateway retrieval of book list for NIV bible
         """
         # GIVEN: A new Bible Gateway extraction class
         handler = BGExtract()
@@ -30,4 +30,43 @@ class TestBibleHTTP(TestCase):
         books = handler.get_books_from_http(u'NIV')
 
         # THEN: We should get back a valid service item
-        assert len(books) == 66, u'The bible should not have had its lenght changed'
+        assert len(books) == 66, u'The bible should not have had any books added or removed'
+
+    def bible_gateway_extract_verse_test(self):
+        """
+        Test the Bible Gateway retrieval of verse list for NIV bible John 3
+        """
+        # GIVEN: A new Bible Gateway extraction class
+        handler = BGExtract()
+
+        # WHEN: The Books list is called
+        results = handler.get_bible_chapter(u'NIV', u'John', 3)
+
+        # THEN: We should get back a valid service item
+        assert len(results.verselist) == 36, u'The bible should not have had any books added or removed'
+
+    def crosswalk_extract_books_test(self):
+        """
+        Test Crosswalk retrieval of book list for NIV bible
+        """
+        # GIVEN: A new Bible Gateway extraction class
+        handler = CWExtract()
+
+        # WHEN: The Books list is called
+        books = handler.get_books_from_http(u'niv')
+
+        # THEN: We should get back a valid service item
+        assert len(books) == 66, u'The bible should not have had any books added or removed'
+
+    def crosswalk_extract_verse_test(self):
+        """
+        Test the Crosswalk retrieval of verse list for NIV bible John 3
+        """
+        # GIVEN: A new Bible Gateway extraction class
+        handler = CWExtract()
+
+        # WHEN: The Books list is called
+        results = handler.get_bible_chapter(u'niv', u'john', 3)
+
+        # THEN: We should get back a valid service item
+        assert len(results.verselist) == 36, u'The bible should not have had any books added or removed'
