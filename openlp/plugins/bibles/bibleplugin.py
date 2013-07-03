d60b7f629b7a57cb45eff542c6b1f884c1ff65e2
Andreas Preikschat <googol@lavabit.com>
2013-07-03 19:06:21 +0200

    reverted change

--- openlp/plugins/bibles/bibleplugin.py
+++ openlp/plugins/bibles/bibleplugin.py
@@ -107,8 +107,6 @@ class BiblePlugin(Plugin):
         Perform tasks on application startup
         """
         Plugin.app_startup(self)
-        # FIXME: python3
-        return
         if self.manager.old_bible_databases:
             if QtGui.QMessageBox.information(self.main_window,
                 translate('OpenLP', 'Information'),
