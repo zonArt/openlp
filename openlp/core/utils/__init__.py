306956be3f1c6666dd39127cab6da0248dcf3d67
Andreas Preikschat <googol@lavabit.com>
2013-07-17 14:22:21 +0200

    fixed image filter

--- openlp/core/utils/__init__.py
+++ openlp/core/utils/__init__.py
@@ -239,7 +239,8 @@ def get_images_filter():
     global IMAGES_FILTER
     if not IMAGES_FILTER:
         log.debug(u'Generating images filter.')
-        formats = map(unicode, QtGui.QImageReader.supportedImageFormats())
+        formats = list(map(bytes, QtGui.QImageReader.supportedImageFormats()))
+        formats = list(map(bytes.decode, formats))
         visible_formats = u'(*.%s)' % u'; *.'.join(formats)
         actual_formats = u'(*.%s)' % u' *.'.join(formats)
         IMAGES_FILTER = u'%s %s %s' % (translate('OpenLP', 'Image Files'), visible_formats, actual_formats)
