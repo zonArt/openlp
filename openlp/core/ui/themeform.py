f568f93976a3c9ad00916c71c41a01c19db5671e
Philip Ridout <phill.ridout@googlemail.com>
2013-07-26 21:17:51 +0100

    actually changed validation this time

--- openlp/core/ui/themeform.py
+++ openlp/core/ui/themeform.py
@@ -179,7 +179,7 @@ class ThemeForm(QtGui.QWizard, Ui_ThemeWizard):
         """
         background_image = BackgroundType.to_string(BackgroundType.Image)
         if self.page(self.currentId()) == self.backgroundPage and \
-                self.theme.background_type == background_image and is_not_image_file(self.imageFileEdit.text()):
+                self.theme.background_type == background_image and is_not_image_file(self.theme.background_filename):
             QtGui.QMessageBox.critical(self, translate('OpenLP.ThemeWizard', 'Background Image Empty'),
                 translate('OpenLP.ThemeWizard', 'You have not selected a '
                     'background image. Please select one before continuing.'))
