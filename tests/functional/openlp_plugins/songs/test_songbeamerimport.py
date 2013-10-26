95baecaaeb96bf00dccbb7295b23c6c16135da90
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2013-10-26 19:25:07 +0200

    Fix vim header (textwidth=120)

--- tests/functional/openlp_plugins/songs/test_songbeamerimport.py
+++ tests/functional/openlp_plugins/songs/test_songbeamerimport.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# vim: autoindent shiftwidth=4 expandtab textwidth=80 tabstop=4 softtabstop=4
+# vim: autoindent shiftwidth=4 expandtab textwidth=120 tabstop=4 softtabstop=4
 
 ###############################################################################
 # OpenLP - Open Source Lyrics Projection                                      #
@@ -26,7 +26,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc., 59  #
 # Temple Place, Suite 330, Boston, MA 02111-1307 USA                          #
 ###############################################################################
-
 """
 This module contains tests for the Songbeamer song importer.
 """
