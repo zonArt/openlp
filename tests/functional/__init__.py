b9027febbbd9b8f4db5d7c3ea3806c4203be0cbb
Raoul Snyman <raoul@snyman.info>
2013-12-18 23:54:56 +0200

    Tidy up some tests

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -11,9 +11,9 @@ import sys
 from PyQt4 import QtGui
 
 if sys.version_info[1] >= 3:
-    from unittest.mock import patch, MagicMock
+    from unittest.mock import MagicMock, patch, mock_open
 else:
-    from mock import patch, MagicMock
+    from mock import MagicMock, patch, mock_open
 
 # Only one QApplication can be created. Use QtGui.QApplication.instance() when you need to "create" a  QApplication.
 application = QtGui.QApplication([])
