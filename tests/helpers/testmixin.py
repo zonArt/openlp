fc13b6e45ac09e1275495060393908cf7b1f1674
Tim Bentley <tim.bentley@gmail.com>
2014-03-14 17:38:57 +0000

    test fixes

--- tests/helpers/testmixin.py
+++ tests/helpers/testmixin.py
@@ -51,5 +51,6 @@ class TestMixin(object):
         Settings().set_filename(self.ini_file)
 
     def destroy_settings(self):
-        os.close(self.fd)
+        if hasattr(self, 'fd'):
+            os.close(self.fd)
         os.unlink(Settings().fileName())
