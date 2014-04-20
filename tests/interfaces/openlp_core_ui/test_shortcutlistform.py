05069e2d3f58a048df559075575c204c26d69f2a
Andreas Preikschat <andreaspreikschat@openlp.org>
2014-04-20 16:03:07 +0200

    added test

---
+++ tests/interfaces/openlp_core_ui/test_shortcutlistform.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# vim: autoindent shiftwidth=4 expandtab textwidth=120 tabstop=4 softtabstop=4
+
+###############################################################################
+# OpenLP - Open Source Lyrics Projection                                      #
+# --------------------------------------------------------------------------- #
+# Copyright (c) 2008-2014 Raoul Snyman                                        #
+# Portions copyright (c) 2008-2014 Tim Bentley, Gerald Britton, Jonathan      #
+# Corwin, Samuel Findlay, Michael Gorven, Scott Guerrieri, Matthias Hub,      #
+# Meinert Jordan, Armin Köhler, Erik Lundin, Edwin Lunando, Brian T. Meyer.   #
+# Joshua Miller, Stevan Pettit, Andreas Preikschat, Mattias Põldaru,          #
+# Christian Richter, Philip Ridout, Simon Scudder, Jeffrey Smith,             #
+# Maikel Stuivenberg, Martin Thompson, Jon Tibble, Dave Warnock,              #
+# Frode Woldsund, Martin Zibricky, Patrick Zimmermann                         #
+# --------------------------------------------------------------------------- #
+# This program is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; version 2 of the License.                              #
+#                                                                             #
+# This program is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with this program; if not, write to the Free Software Foundation, Inc., 59  #
+# Temple Place, Suite 330, Boston, MA 02111-1307 USA                          #
+###############################################################################
+"""
+Package to test the openlp.core.ui.shortcutform package.
+"""
+from unittest import TestCase
+
+from PyQt4 import QtCore, QtGui, QtTest
+
+from openlp.core.common import Registry
+from openlp.core.ui.shortcutlistform import ShortcutListForm
+from tests.interfaces import patch
+from tests.helpers.testmixin import TestMixin
+
+
+class TestShortcutform(TestCase, TestMixin):
+
+    def setUp(self):
+        """
+        Create the UI
+        """
+        Registry.create()
+        self.get_application()
+        self.main_window = QtGui.QMainWindow()
+        Registry().register('main_window', self.main_window)
+        self.form = ShortcutListForm()
+
+    def tearDown(self):
+        """
+        Delete all the C++ objects at the end so that we don't have a segfault
+        """
+        del self.form
+        del self.main_window
+
+    def adjust_button_test(self):
+        """
+        Test the _adjust_button() method
+        """
+        # GIVEN: A button.
+        button = QtGui.QPushButton()
+        checked= True
+        enabled = True
+        text = "new!"
+
+        # WHEN: Call the method.
+        with patch('PyQt4.QtGui.QPushButton.setChecked') as mocked_check_method:
+            self.form._adjust_button(button, checked, enabled, text)
+
+
+            # THEN: The button should be changed.
+            self.assertEqual(button.text(), text, "The text should match.")
+            mocked_check_method.assert_called_once_with(True)
+            self.assertEqual(button.isEnabled(), enabled, "The button should be disabled.")
