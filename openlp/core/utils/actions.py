f881057d5b3c3f10e3fcb1fee767eb596f34fd57
Phill Ridout <phill.ridout@gmail.com>
2014-08-26 18:38:07 +0100

    PEP fixes

--- openlp/core/utils/actions.py
+++ openlp/core/utils/actions.py
@@ -280,7 +280,7 @@ class ActionList(object):
                 ActionList.shortcut_map[shortcuts[1]] = actions
             else:
                 log.warning('Shortcut "%s" is removed from "%s" because another action already uses this shortcut.' %
-                         (shortcuts[1], action.objectName()))
+                            (shortcuts[1], action.objectName()))
                 shortcuts.remove(shortcuts[1])
         # Check the primary shortcut.
         existing_actions = ActionList.shortcut_map.get(shortcuts[0], [])
@@ -291,7 +291,7 @@ class ActionList(object):
             ActionList.shortcut_map[shortcuts[0]] = actions
         else:
             log.warning('Shortcut "%s" is removed from "%s" because another action already uses this shortcut.' %
-                     (shortcuts[0], action.objectName()))
+                        (shortcuts[0], action.objectName()))
             shortcuts.remove(shortcuts[0])
         action.setShortcuts([QtGui.QKeySequence(shortcut) for shortcut in shortcuts])
 
