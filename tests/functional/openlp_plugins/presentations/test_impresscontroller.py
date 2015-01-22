2a9a5ecf5ba898bcedec836162b0370830de3ec4
Phill Ridout <phill.ridout@gmail.com>
2015-01-22 17:52:48 +0000

    test fixes

--- tests/functional/openlp_plugins/presentations/test_impresscontroller.py
+++ tests/functional/openlp_plugins/presentations/test_impresscontroller.py
@@ -31,8 +31,10 @@ from tests.functional import patch, MagicMock
 from tests.utils.constants import TEST_RESOURCES_PATH
 from tests.helpers.testmixin import TestMixin
 
+from openlp.core.common import Settings
 from openlp.plugins.presentations.lib.impresscontroller import \
     ImpressController, ImpressDocument, TextType
+from openlp.plugins.presentations.presentationplugin import __default_settings__
 
 
 class TestImpressController(TestCase, TestMixin):
@@ -79,6 +81,7 @@ class TestImpressDocument(TestCase):
     def setUp(self):
         mocked_plugin = MagicMock()
         mocked_plugin.settings_section = 'presentations'
+        Settings().extend_default_settings(__default_settings__)
         self.file_name = os.path.join(TEST_RESOURCES_PATH, 'presentations', 'test.pptx')
         self.ppc = ImpressController(mocked_plugin)
         self.doc = ImpressDocument(self.ppc, self.file_name)
