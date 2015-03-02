146543517a7556c62ad3661654a8dd1e97c48061
Raoul Snyman <raoul@snyman.info>
2015-03-02 21:21:41 +0200

    Fix code analysis

--- tests/functional/openlp_plugins/songs/test_editverseform.py
+++ tests/functional/openlp_plugins/songs/test_editverseform.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+# vim: autoindent shiftwidth=4 expandtab textwidth=120 tabstop=4 softtabstop=4
+
+###############################################################################
+# OpenLP - Open Source Lyrics Projection                                      #
+# --------------------------------------------------------------------------- #
+# Copyright (c) 2008-2015 OpenLP Developers                                   #
+# --------------------------------------------------------------------------- #
+# This program is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; version 2 of the License.                              #
+#                                                                             #
+# This program is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with this program; if not, write to the Free Software Foundation, Inc., 59  #
+# Temple Place, Suite 330, Boston, MA 02111-1307 USA                          #
+###############################################################################
 """
 This module contains tests for the editverseform of the Songs plugin.
 """
@@ -38,12 +59,12 @@ class TestEditVerseForm(TestCase, TestMixin):
         """
         # GIVEN some input values
         self.edit_verse_form.has_single_verse = True
-        self.edit_verse_form.verse_type_combo_box.currentIndex = MagicMock(return_value = 0)
-        self.edit_verse_form.verse_text_edit.toPlainText = MagicMock(return_value = 'Text')
+        self.edit_verse_form.verse_type_combo_box.currentIndex = MagicMock(return_value=0)
+        self.edit_verse_form.verse_text_edit.toPlainText = MagicMock(return_value='Text')
         self.edit_verse_form.verse_number_box.setValue(3)
-        
+
         # WHEN the method is called
         self.edit_verse_form.update_suggested_verse_number()
-        
+
         # THEN the verse number must not be changed
         self.assertEqual(3, self.edit_verse_form.verse_number_box.value(), 'The verse number should be 3')
