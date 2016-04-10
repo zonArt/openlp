a8b9861af2045401aba13b9c0cdf3fd991da0672
suutari-olli <suutari.olli@gmail.com>
2016-04-10 03:12:18 +0300

    Hopefully fixed the test for checking the main window name...

--- tests/functional/openlp_core_ui/test_mainwindow.py
+++ tests/functional/openlp_core_ui/test_mainwindow.py
@@ -108,9 +108,9 @@ class TestMainWindow(TestCase, TestMixin):
 
         # WHEN no changes are made to the service
 
-        # THEN the main window's title shoud be the same as the OLPV2x string in the UiStrings class
-        self.assertEqual(self.main_window.windowTitle(), UiStrings().OLPV2x,
-                         'The main window\'s title should be the same as the OLPV2x string in UiStrings class')
+        # THEN the main window's title shoud be the same as the OLP string in the UiStrings class
+        self.assertEqual(self.main_window.windowTitle(), UiStrings().OLP,
+                         'The main window\'s title should be the same as the OLP string in UiStrings class')
 
     def set_service_modifed_test(self):
         """
@@ -122,8 +122,8 @@ class TestMainWindow(TestCase, TestMixin):
         self.main_window.set_service_modified(True, 'test.osz')
 
         # THEN the main window's title should be set to the
-        self.assertEqual(self.main_window.windowTitle(), '%s - %s*' % (UiStrings().OLPV2x, 'test.osz'),
-                         'The main window\'s title should be set to "<the contents of UiStrings().OLPV2x> - test.osz*"')
+        self.assertEqual(self.main_window.windowTitle(), '%s - %s*' % (UiStrings().OLP, 'test.osz'),
+                         'The main window\'s title should be set to "<the contents of UiStrings().OLP> - test.osz*"')
 
     def set_service_unmodified_test(self):
         """
@@ -135,8 +135,8 @@ class TestMainWindow(TestCase, TestMixin):
         self.main_window.set_service_modified(False, 'test.osz')
 
         # THEN the main window's title should be set to the
-        self.assertEqual(self.main_window.windowTitle(), '%s - %s' % (UiStrings().OLPV2x, 'test.osz'),
-                         'The main window\'s title should be set to "<the contents of UiStrings().OLPV2x> - test.osz"')
+        self.assertEqual(self.main_window.windowTitle(), '%s - %s' % (UiStrings().OLP, 'test.osz'),
+                         'The main window\'s title should be set to "<the contents of UiStrings().OLP> - test.osz"')
 
     def mainwindow_configuration_test(self):
         """
