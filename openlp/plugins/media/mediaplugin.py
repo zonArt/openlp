df22f9be5601c67ebb272fa91afb9f17d0afd9ce
Ken Roberts <alisonken1@gmail.com>
2016-04-23 14:12:28 -0700

    Fix extraneous print() in media plugin

--- openlp/plugins/media/mediaplugin.py
+++ openlp/plugins/media/mediaplugin.py
@@ -160,7 +160,8 @@ def process_check_binary(program_path):
     """
     program_type = None
     runlog = check_binary_exists(program_path)
-    print(runlog, type(runlog))
+    # NOTE: Unneeded print statement - let originator clear it out if needed
+    # print(runlog, type(runlog))
     # Analyse the output to see it the program is mediainfo
     for line in runlog.splitlines():
         decoded_line = line.decode()
