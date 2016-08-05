e418e376e07b184f50e3f1c23feb38b88e95d039
Ken Roberts <alisonken1@gmail.com>
2016-08-05 12:57:25 -0700

    pep8

--- openlp/plugins/songs/lib/openlyricsxml.py
+++ openlp/plugins/songs/lib/openlyricsxml.py
@@ -643,11 +643,7 @@ class OpenLyrics(object):
         # Append text from tail and add formatting end tag.
         # TODO: Verify format() with template variables
         if element.tag == NSMAP % 'tag' and use_endtag:
-<<<<<<< TREE
             text += '{{/{name}}}'.format(name=element.get('name'))
-=======
-            text += '{{{name}}}'.format(name=element.get('name'))
->>>>>>> MERGE-SOURCE
         # Append text from tail.
         if element.tail:
             text += element.tail
