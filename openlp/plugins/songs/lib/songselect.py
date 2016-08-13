de107d50aa1d0898fe3dfa6afe54ffa89e398d90
Raoul Snyman <raoul@snyman.info>
2016-08-13 16:50:15 +0200

    Fix another pep8 issues

--- openlp/plugins/songs/lib/songselect.py
+++ openlp/plugins/songs/lib/songselect.py
@@ -37,7 +37,7 @@ from openlp.plugins.songs.lib import Song, Author, Topic, VerseType, clean_song
 from openlp.plugins.songs.lib.openlyricsxml import SongXML
 
 USER_AGENTS = [
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
     'Chrome/52.0.2743.116 Safari/537.36',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36',
     'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0',
